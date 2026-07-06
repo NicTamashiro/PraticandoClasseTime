301c44a2b948edc904a553269418be1a386ca261
Nicolas <adilsonq1976@gmail.com>
2026-07-06 10:43:41 -0300

    Initial commit

---
+++ src/PrimeiroContatoComClasseTime/Main.java
@@ -0,0 +1,24 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate dataCompra = LocalDate.now();
+        LocalDate dataPrimeiraParcela = LocalDate.of(2026, 8,7);
+        LocalDate dataSegundaParcela = dataPrimeiraParcela.plusDays(30);
+
+        if(dataPrimeiraParcela.isBefore(LocalDate.now())){
+            System.out.println("Anterior dia do vencimento");
+        } else {
+            System.out.println("Superior dia do vencimento");
+        }
+
+        System.out.println("Data compra: " + dataCompra);
+        System.out.println("Data primeira parcela: " + dataPrimeiraParcela);
+        System.out.println("Data segunda parcela: " + dataSegundaParcela);
+
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Data compra formatada: " + dataCompra.format(formato));
+    }
+}
